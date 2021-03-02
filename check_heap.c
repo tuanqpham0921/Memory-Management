1ed5e9890bd82fd630f41683130ab2576b1b02ab
Tuan Pham <tqp85@cs.utexas.edu>
2021-03-01 18:13:11 -0600

    files

---
+++ check_heap.c
@@ -0,0 +1,15 @@
+
+#include "umalloc.h"
+
+//Place any variables needed here from umalloc.c as an extern.
+extern memory_block_t *free_head;
+
+/*
+ * check_heap -  used to check that the heap is still in a consistent state.
+ * Required to be completed for checkpoint 1.
+ * Should return 0 if the heap is still consistent, otherwise return a non-zero
+ * return code. Asserts are also a useful tool here.
+ */
+int check_heap() {
+    return 0;
+}
